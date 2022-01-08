c987b12e7de969a24ffc6bfd482482bab7f2dab8
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-01-08 21:36:30 +0700

    chore: intialize configuration
    - dependencies
    - formatting
    - remove unused code

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  semi: false,
+  sortClassNamesUnknownClassesSeparator: '',
+  sortClassNamesPrefixes:
+    'xs:,sm:,md:,lg:,xl:,2xl:,dark:,motion-safe:,motion-reduce:,first:,last:,odd:,even:,visited:,checked:,group-hover:,group-focus:,focus-within:,hover:,focus:,focus-visible:,active:,disabled:',
+  sortClassNamesSortFunctions: 'clsx,classNames,cx,clsxm'
+}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/***/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    colors: {
+      blue: '#1F36FF',
+      purple: '#a800ff',
+      pink: '#FF1FA6',
+      orange: '#FF781F',
+      green: '#A6FF1F',
+      yellow: '#FFE81F',
+      'gray-dark': '#413f30',
+      gray: '#8492a6',
+      'gray-light': '#d3dce6'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem'
+      },
+      borderRadius: {
+        '4xl': '2rem'
+      }
+    }
+  }
+}
