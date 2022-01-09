7eb2ae4ae3887dc9063a2ee7aec8d5c0b1ff90c5
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-01-09 20:42:17 +0700

    refactor: header
    - add nav links
    - add class names
    - use next/link in starwars logo

--- components/molecules/header/header.jsx
+++ components/molecules/header/header.jsx
@@ -1,14 +1,49 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
+export const LINKS = [
+  {
+    label: 'Movie',
+    href: '/films'
+  },
+  {
+    label: 'People',
+    href: '/people'
+  },
+  {
+    label: 'Planet',
+    href: '/planet'
+  },
+  {
+    label: 'Species',
+    href: '/species'
+  },
+  {
+    label: 'Starship',
+    href: 'starship'
+  }
+]
 
 export default function Header() {
   return (
-    <div className='bg-black'>
-      <Image
-        width={200}
-        height={100}
-        src='/img/brand.png'
-        alt='starwars banner'
-      />
-    </div>
+    <section className='bg-gray-dark border-b-2 border-gray flex gap-5 items-center justify-content-start pl-5 py-5'>
+      <Link href='/'>
+        <a>
+          <Image
+            width={75}
+            height={50}
+            layout='fixed'
+            src='/img/brand.png'
+            alt='starwars banner'
+          />
+        </a>
+      </Link>
+
+      {LINKS.map((link, key) => (
+        <Link key={key} href={link.href}>
+          {link.label}
+        </Link>
+      ))}
+    </section>
   )
 }
