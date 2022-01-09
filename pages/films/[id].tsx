0af28188944c2b61ca7623490f49010f344b13fe
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-01-09 21:16:29 +0700

    feature: dynamic film page
    - add new film queries
    - implement film-profile and people-section

---
+++ pages/films/[id].tsx
@@ -0,0 +1,77 @@
+//import { gql } from '@apollo/client'
+import type { GetStaticPropsContext, NextPage } from 'next'
+
+import client from '@/lib/apollo-client'
+
+import FilmProfile from '@/organisms/film-profile'
+import PeopleSection from '@/organisms/people-section'
+
+import { GET_FILM_DETAIL, GET_FILM_IDS } from '@/queries/film-queries'
+
+import { Film, FilmEdge } from '@/types/film'
+
+interface FilmDetailProps {
+  film: Film
+}
+
+const Home: NextPage<FilmDetailProps> = ({ film }: FilmDetailProps) => {
+  const {
+    characterConnection: { characters },
+    ...filmDetailProps
+  } = film
+
+  const { title, openingCrawl, episodeID, director, producers } =
+    filmDetailProps
+
+  return (
+    <main className='m-5'>
+      <FilmProfile
+        title={title}
+        openingCrawl={openingCrawl}
+        episodeID={episodeID}
+        director={director}
+        producers={producers}
+      />
+
+      <PeopleSection people={characters} />
+    </main>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const { data } = await client.query({ query: GET_FILM_IDS })
+
+  const edges = data?.allFilms?.edges || []
+  const paths = edges.map((edge: FilmEdge) => ({
+    params: {
+      id: `${edge.node.id}`
+    }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const id = context.params?.id
+  if (id) {
+    const { data } = await client.query({
+      query: GET_FILM_DETAIL,
+      variables: { id }
+    })
+
+    return {
+      props: {
+        film: data.film
+      },
+      revalidate: 1
+    }
+  }
+  return {
+    notFound: true
+  }
+}
+
+export default Home
--- queries/film-queries.ts
+++ queries/film-queries.ts
@@ -11,3 +11,51 @@ export const GET_FILMS = gql`
     }
   }
 `
+
+export const GET_FILM_IDS = gql`
+  query getFilmIds {
+    allFilms {
+      totalCount
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+`
+
+export const GET_FILM_DETAIL = gql`
+  query getFilmDetail($id: ID!) {
+    film(id: $id) {
+      id
+      title
+      episodeID
+      openingCrawl
+      producers
+      director
+      characterConnection {
+        characters {
+          name
+          species {
+            id
+            classification
+            name
+          }
+        }
+      }
+      starshipConnection {
+        starships {
+          id
+          name
+        }
+      }
+      planetConnection {
+        planets {
+          name
+          gravity
+        }
+      }
+    }
+  }
+`
