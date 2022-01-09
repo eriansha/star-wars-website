4bf76f989980e28d29f7026c70f8ae37be48d3d5
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-01-09 21:11:27 +0700

    feature: film poster

---
+++ components/organisms/film-poster-section/film-poster-section.tsx
@@ -0,0 +1,28 @@
+import FilmCard from '@/molecules/film-card'
+
+import { Film } from '@/types/film'
+
+interface FilmPosterProps {
+  films: Film[]
+}
+
+export default function FilmPosterSection({ films }: FilmPosterProps) {
+  return (
+    <section className='bg-gray-dark pb-24 pt-24'>
+      <div className='flex flex-col items-center'>
+        <h1 className='pb-16 text-5xl'>Movie</h1>
+
+        <div className='flex flex-wrap gap-10 items-center justify-evenly mx-8'>
+          {films.map((film, key) => (
+            <FilmCard
+              key={key}
+              filmId={film.id}
+              title={film.title}
+              episodeID={film.episodeID}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
