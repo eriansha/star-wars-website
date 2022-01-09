f57bbbfc311db3a54a4c6e3bf1459bdae005d17d
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-01-09 11:02:49 +0700

    chore: add snapshot resolver

---
+++ snapshotResolver.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testPathForConsistencyCheck: 'some/__tests__/example.test.js',
+
+  /** resolves from test to snapshot path */
+  resolveSnapshotPath: (testPath, snapshotExtension) => {
+    return testPath.replace('src/', '__snapshots__/') + snapshotExtension
+  },
+
+  /** resolves from snapshot to test path */
+  resolveTestPath: (snapshotFilePath, snapshotExtension) => {
+    return snapshotFilePath
+      .replace('__snapshots__/', 'src/')
+      .slice(0, -snapshotExtension.length)
+  }
+}
