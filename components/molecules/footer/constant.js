b31e1c06cf8a067baff0d2a04936545c5f10fd81
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-01-09 11:00:26 +0700

    feature: footer

---
+++ components/molecules/footer/constant.js
@@ -0,0 +1,35 @@
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
+export const LINKS = [
+  {
+    label: 'People',
+    href: '/people'
+  },
+  {
+    label: 'Planet',
+    href: '/planet'
+  },
+  {
+    label: 'Species',
+    href: '/species'
+  },
+  {
+    label: 'Starship',
+    href: 'starship'
+  }
+]
+
+export const SOCIAL_MEDIAS = [
+  {
+    href: 'https://github.com/eriansha',
+    label: <FaGithub />
+  },
+  {
+    href: 'https://www.linkedin.com/in/eriansha/',
+    label: <FaLinkedin />
+  },
+  {
+    href: 'https://twitter.com/mivanputra',
+    label: <FaTwitter />
+  }
+]
---
+++ components/molecules/footer/footer.jsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import React from 'react'
+
+import { LINKS, SOCIAL_MEDIAS } from './constant'
+
+export default function Footer() {
+  return (
+    <footer className='bg-black h-48 mt-4 pb-2 w-full'>
+      <main className='border-t flex flex-col h-full items-center layout pt-6'>
+        <p data-testid='links' className='flex flex-wrap gap-x-8 gap-y-4 justify-center'>
+          {LINKS.map((link, key) => (
+            <Link key={key} href={link.href}>
+              {link.label}
+            </Link>
+          ))}
+        </p>
+        <div data-testid='social-medias' className='flex mt-5 space-x-4'>
+          {SOCIAL_MEDIAS.map((socialMedia, key) => (
+            <a
+              key={key}
+              href={socialMedia.href}
+              rel='noreferrer'
+              target='_blank'
+            >
+              {socialMedia.label}
+            </a>
+          ))}
+        </div>
+        <p data-testid='author' className='mt-8 text-gray-600 text-sm dark:text-gray-300'>
+          {'© Muhamad Ivan Putra Eriansya 2022'}
+        </p>
+      </main>
+    </footer>
+  )
+}
